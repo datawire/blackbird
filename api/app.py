f7a58019b5520aeb1249205f4ca2ddd57beb06e3
Richard Li <richard@datawire.io>
2018-01-25 17:15:32 -0500

    formatting

--- api/app.py
+++ api/app.py
@@ -21,7 +21,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Welcome Jean-Paul! (up %s, %s)\n" % (elapsed(), os.environ["BUILD_PROFILE"])
+    return "%s (%s)\n" % (elapsed(), os.environ["BUILD_PROFILE"])
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
